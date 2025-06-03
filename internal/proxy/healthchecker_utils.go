632de2c2c6bd4ad3b13e92e4f190bca0c4050ed5
kewl fft <kewl@alto.eu.org>
2025-06-03 15:50:56 +0100

    Refactor gas left call request construction in healthchecker_utils.go to use a string literal for improved readability

--- internal/proxy/healthchecker_utils.go
+++ internal/proxy/healthchecker_utils.go
@@ -26,35 +26,33 @@ func hexToUint(hexString string) (uint64, error) {
 }
 
 func performGasLeftCall(c context.Context, client *http.Client, url string) (uint64, error) {
-	var gasLeftCallRaw = bytes.NewBufferString(`
-{
-    "method": "eth_call",
-    "params": [
-        {
-            "from": "0xab5801a7d398351b8be11c439e05c5b3259aec9b",
-            "to": "0x5555555555555555555555555555555555555555",
-            "value": "0x0",
-            "data": "0x51be4eaa",
-            "gas": "0x3B9ACA00"
-        },
-        "latest",
-        {
-            "0x5555555555555555555555555555555555555555": {
-                "code": "0x6080604052348015600f57600080fd5b506004361060285760003560e01c806351be4eaa14602d575b600080fd5b60336045565b60408051918252519081900360200190f35b60005a90509056fea2646970667358221220b8fc97f4ae43b2849771c773ac6e7040e00be6910c96cabe366b34c3f294d27764736f6c634300060c0033"
+	gasLeftCallRaw := `{
+        "method": "eth_call",
+        "params": [
+            {
+                "from": "0xab5801a7d398351b8be11c439e05c5b3259aec9b",
+                "to": "0x5555555555555555555555555555555555555555",
+                "value": "0x0",
+                "data": "0x51be4eaa",
+                "gas": "0x3B9ACA00"
+            },
+            "latest",
+            {
+                "0x5555555555555555555555555555555555555555": {
+                    "code": "0x6080604052348015600f57600080fd5b506004361060285760003560e01c806351be4eaa14602d575b600080fd5b60336045565b60408051918252519081900360200190f35b60005a90509056fea2646970667358221220b8fc97f4ae43b2849771c773ac6e7040e00be6910c96cabe366b34c3f294d27764736f6c634300060c0033"
+                }
             }
-        }
-    ],
-    "id": 1,
-    "jsonrpc": "2.0"
-}
-`)
+        ],
+        "id": 1,
+        "jsonrpc": "2.0"
+    }`
 
-	r, err := http.NewRequestWithContext(c, http.MethodPost, url, gasLeftCallRaw)
+	r, err := http.NewRequestWithContext(c, http.MethodPost, url, bytes.NewBufferString(gasLeftCallRaw))
 	if err != nil {
 		return 0, fmt.Errorf("performGasLeftCall: NewRequestWithContext error: %w", err)
 	}
 
-	r.Header.Add(headers.ContentType, "application/json")
+	r.Header.Set(headers.ContentType, "application/json")
 	r.Header.Set(headers.UserAgent, userAgent)
 
 	resp, err := client.Do(r)
